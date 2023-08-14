e8f934f1e05fd0fc4d372e8ed4386b909b4bece4
rafeezk <rayzaqqq@gmail.com>
2023-08-14 13:39:41 +0700

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return <footer className='bg-primary py-12'>
     <div className="container mx-auto">
-      <p className="text-white text-center">Copyright &copy;2023 Muhammad Fathul Falah. All right reserved.</p>
+      <p className="text-white text-center">Copyright &copy;2023 rfiazky. All right reserved.</p>
     </div>
   </footer>;
 };
